bbdb436b51fed0786e03f88683f3eaee1b49c424
kaahoza <kaahoza@gmail.com>
2026-07-23 21:52:42 +0200

    Initial commit

---
+++ e-commerce-microservices/src/test/java/com/anele/e_commerce_microservices/ECommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anele.e_commerce_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
